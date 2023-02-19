a71465bcd45250f34e08714b6e0a9e96577eeaf1
HenryIII <h.soberanoiii@gmail.com>
2023-02-20 01:43:07 +0800

    hackademy

--- hackademy/activity1/models.py
+++ hackademy/activity1/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    name = models.CharField(max_length=100)
+    user = models.OneToOneField(
+        User, 
+        on_delete = models.CASCADE, null=True,
+    
+    )
+#    name = models.CharField(max_length=100)
     profile_picture = models.ImageField()
     description = models.TextField(max_length=250)
